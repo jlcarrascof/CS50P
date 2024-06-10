7c3c130666d4a7ef7e8073b664c5286dbc5fadbd
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-10 19:07:11 -0400

    refactor: Capitalize and title case user's name in hello.py

--- 00-Functions-Variables/hello.py
+++ 00-Functions-Variables/hello.py
@@ -7,13 +7,8 @@ This is a multiline comment
 """
 
 # Remove whitespace from str
-name = name.strip()
+name = name.strip().title()
 
-# Capitalize user's name
-name = name.capitalize()
-
-# Capitalize all the first letters
-name = name.title()
 
 print(f"Hello, {name}")
 
