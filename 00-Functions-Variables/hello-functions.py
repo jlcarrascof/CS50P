25b32ff08a940af71d98ccb8fa8b3c947fe458a4
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-11 12:24:26 -0400

    refactor: Update hello-functions.py to use default argument for 'name' parameter in hello function

--- 00-Functions-Variables/hello-functions.py
+++ 00-Functions-Variables/hello-functions.py
@@ -1,7 +1,24 @@
-def hello(to="world"):
-    print("Hello,", to)
-
-
 hello()
 name = input("What's your name? ")
 hello(name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def hello(name="world"):
+    print("Hello,", name)
