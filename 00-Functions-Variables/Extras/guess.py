1a1b7632359c1ce2e8c0faec1611a070982ae178
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-17 19:11:54 -0400

    refactor: Update guess.py to use main function and prompt user for guess

--- 00-Functions-Variables/Extras/guess.py
+++ 00-Functions-Variables/Extras/guess.py
@@ -1,10 +1,10 @@
 def get_guess():
-    guess = int(input("Enter a guess: "))
+    guess = input("Enter a guess: ")
     return guess
 
 def main():
     guess = get_guess()
-    if guess == 50:
+    if guess == "fifty":
         print("Correct!")
     else:
         print("Incorrect!")
