d3ba929b789da9cedfa1bcf99122aa997aaf0540
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-06-19 11:55:58 -0400

    refactor: Add main function to calculate energy in einstein.py

--- 00-Functions-Variables/Exercises/einstein.py
+++ 00-Functions-Variables/Exercises/einstein.py
@@ -1,3 +1,11 @@
 # Exercise IV - Lesson 00 - Functions and Variables
 
 # Einstein
+
+def main():
+    m = int((input("M: ")))
+    c = 300000000
+    e = m * (c ** 2)
+    print("E =" , e)
+
+main()
